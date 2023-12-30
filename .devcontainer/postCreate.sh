ce3f8103416bbe88709b88d9f57295578b5ebb57
Junya Miyamoto <21972700+m1yam0t0@users.noreply.github.com>
2023-12-31 01:51:27 +0900

    Initial commit

---
+++ .devcontainer/postCreate.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# Install aqua
+curl -sSfL -O https://raw.githubusercontent.com/aquaproj/aqua-installer/v2.2.0/aqua-installer
+echo "d13118c3172d90ffa6be205344b93e8621de9bf47c852d80da188ffa6985c276  aqua-installer" | sha256sum -c
+chmod +x aqua-installer
+./aqua-installer
+rm -f aqua-installer
+
+# Install tools
+aqua i -l
